eb6efa9e686dcbb0d0d8489a14cf67b53b8b9acf
silviacasavola <silcasavola@gmail.com>
2020-11-24 23:49:31 +0100

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -44,12 +44,12 @@ function draw() {
   image(img1, windowWidth/3, windowHeight/4, 210, 210);
   image(img2, windowWidth/3, windowHeight/4, 210, 210);
   image(img3, windowWidth/3, windowHeight/4, 210, 210);
-  image(img4, windowWidth/2, windowHeight/3, 210, 210);
-  image(img5, windowWidth/2, windowHeight/3, 210, 210);
-  image(img6, windowWidth/2, windowHeight/3, 210, 210);
-  image(img7, (windowWidth/3)*2, (windowHeight/3)*2, 210, 210);
-  image(img8, (windowWidth/3)*2, (windowHeight/3)*2, 210, 210);
-  image(img9, (windowWidth/3)*2, (windowHeight/3)*2, 210, 210);
+  image(img4, windowWidth/2, windowHeight/2, 210, 210);
+  image(img5, windowWidth/2, windowHeight/2, 210, 210);
+  image(img6, windowWidth/2, windowHeight/2, 210, 210);
+  image(img7, (windowWidth/3)*2, (windowHeight/4)*3, 210, 210);
+  image(img8, (windowWidth/3)*2, (windowHeight/4)*3, 210, 210);
+  image(img9, (windowWidth/3)*2, (windowHeight/4)*3, 210, 210);
 
 
   let message = {
